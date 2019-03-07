9cf5e1be6f5661d35b46543b0008908fa30a183e
Dâmi Henrique Pelegrino <damipelegrino@inatel.br>
2019-03-07 18:32:24 -0300

    first commit

---
+++ backend/config/database.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+module.exports = mongoose.connect('mongodb://localhost/db_finance', { useNewUrlParser: true })
+
+mongoose.Error.messages.general.required = "O atributo {PATH} é obrigatório"
